904d5a1f65d6bb4eb726b2dda779fc74e2ce328e
Tiago Henrique <tiagomacedoh015@gmail.com>
2023-07-21 15:24:34 -0300

    Atualizando

--- src/js/interacao-imc.js
+++ src/js/interacao-imc.js
@@ -18,43 +18,43 @@ function calcularImc(){
         } else {
                 imc = peso / (altura * altura )
                 textoResultado.style.marginTop = "60px"
-
-        if (imc < 10){
-                textoResultado.classList.add("animacao")
-                textoResultado.innerHTML = `Ops, confira se você digitou as informações corretamente!`
-                textoResultado.style.color = "red"
+        
+                if (imc < 10){
+                        textoResultado.classList.add("animacao")
+                        textoResultado.innerHTML = `Ops, confira se você digitou as informações corretamente!`
+                        textoResultado.style.color = "red"
+                }
+                else if (imc < 18.5){
+                        textoResultado.classList.add("animacao")
+                        textoResultado.style.color = "red"
+                        textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está ABAIXO DO PESO`
+                }
+                else if (imc >= 18.5 && imc <= 24.9){
+                        textoResultado.classList.add("animacao")
+                        textoResultado.style.color = "green"
+                        textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está com seu peso NORMAL`
+                }
+                else if (imc >= 25 && imc <= 29.9){
+                        textoResultado.classList.add("animacao")
+                        textoResultado.style.color = "red"
+                        textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está com SOBREPESO`
+                }
+                else if (imc >= 30 && imc <= 34.9){
+                        textoResultado.classList.add("animacao")
+                        textoResultado.style.color = "red"
+                        textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está com OBESIDADE DE PRIMEIRO GRAU`
+                }
+                else if (imc >= 35 && imc <= 39.9){
+                        textoResultado.classList.add("animacao")
+                        textoResultado.style.color = "red"
+                        textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está com OBESIDADE DE SEGUNDO GRAU`
+                }
+                else if (imc > 40){
+                        textoResultado.classList.add("animacao")
+                        textoResultado.style.color = "red"
+                        textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está com OBESIDADE GRAVE`
+                }
         }
-        else if (imc < 18.5){
-                textoResultado.classList.add("animacao")
-                textoResultado.style.color = "red"
-                textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está ABAIXO DO PESO`
-        }
-        else if (imc >= 18.5 && imc <= 24.9){
-                textoResultado.classList.add("animacao")
-                textoResultado.style.color = "green"
-                textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está com seu peso NORMAL`
-        }
-        else if (imc >= 25 && imc <= 29.9){
-                textoResultado.classList.add("animacao")
-                textoResultado.style.color = "red"
-                textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está com SOBREPESO`
-        }
-        else if (imc >= 30 && imc <= 34.9){
-                textoResultado.classList.add("animacao")
-                textoResultado.style.color = "red"
-                textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está com OBESIDADE DE PRIMEIRO GRAU`
-        }
-        else if (imc >= 35 && imc <= 39.9){
-                textoResultado.classList.add("animacao")
-                textoResultado.style.color = "red"
-                textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está com OBESIDADE DE SEGUNDO GRAU`
-        }
-        else if (imc > 40){
-                textoResultado.classList.add("animacao")
-                textoResultado.style.color = "red"
-                textoResultado.innerHTML = `O resultado do cálculo do seu IMC é de ${imc.toFixed(2)} você está com OBESIDADE GRAVE`
-        }
-    }
 }
 
 
